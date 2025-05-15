b8551588ccd99d610f9292a4f1c76f3953d44f2a
Jihed ZGHAL <jzghal@parisnanterre.fr>
2025-05-15 15:16:58 +0200

    Update PhaFiDyn_Branch_Original_CZM.py

--- PhaFiDyn_Branch_Original_CZM.py
+++ PhaFiDyn_Branch_Original_CZM.py
@@ -198,7 +198,7 @@ def postprocessing(Incr, Res_folder, base_file, Data):
     elastic_energy_value = assemble(Elastic_energy)
     kinetic_energy_value = assemble(Kinetic_energy)
     surface_energy_value = assemble(Dissipated_energy)
-    external_energy_value = assemble(External_energy)
+    external_energy_value = ext_energy
 
     total_energy_value = surface_energy_value + elastic_energy_value + kinetic_energy_value - external_energy_value  
     
@@ -1171,6 +1171,7 @@ Data = []  # List to store simulation data
 # Initialization
 Incr = 0 
 t = Constant(0)
+ext_energy = 0.
 
 # Loop over the loads
 errphi  = Function(Vdam)
@@ -1219,6 +1220,8 @@ while float(t) < T:
     
     timestep = time.process_time() - start
     
+    ext_energy += float(dt)*assemble(External_energy)
+
     # Perform post-processing and data storage
     postprocessing(Incr, Res_folder, base_file, Data)
     
@@ -1243,8 +1246,3 @@ liste.append(str(RunningTime))
 with open(Res_folder + base_file + 'RunningTime.dat', 'w') as filout:
     for lst in liste:
         filout.write(f"{lst}")
-
-        
-
-     
-     
